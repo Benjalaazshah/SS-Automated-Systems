92d30d9fd075bfe63a440ad400c6abe2596e014a
Benjalaazshah <Benjalaazshah@gmail.com>
2024-03-05 19:57:43 -0600

    Added essential starting files.

---
+++ Sources/index.ts
@@ -0,0 +1,14 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import ENV from "dotenv";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+ENV.config();
+
+// EVENTS
+
+client.on(Events.ClientReady, (readyClient) => {
+	console.log(`Client is ready! Tag: ${readyClient.user.tag}.`);
+});
+
+client.login(process.env.TOKEN);
